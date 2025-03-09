82981de83f4a9826e60f900c3323463fba2ff890
Maxijana <83040029+Maxijana@users.noreply.github.com>
2025-03-09 18:57:49 +0100

    Add files via upload

--- leowebstranica.js
+++ leowebstranica.js
@@ -1,4 +1,4 @@
-leowebstranica.js
+//leowebstranica.js
 
 // Provjera da li je JS datoteka ispravno učitana
 console.log('JS učitan');
@@ -9,9 +9,15 @@ document.getElementById("kontaktForm").addEventListener("submit", function(event
 
     console.log("Forma je poslana.");  // Ovdje ćemo vidjeti ako je forma poslana
 
-    // Prikazivanje poruke zahvalnosti
-    document.getElementById("formMessage").style.display = "block";
-    console.log("Poruka se prikazuje."); // Provjeriti da li se poruka prikazuje
+    // Provjera ako je poruka s ID-om 'formMessage' ispravno pronađena
+    var formMessage = document.getElementById("formMessage");
+    if (formMessage) {
+        // Prikazivanje poruke zahvalnosti
+        formMessage.style.display = "block";
+        console.log("Poruka se prikazuje."); // Provjeriti da li se poruka prikazuje
+    } else {
+        console.log("Element 'formMessage' nije pronađen.");
+    }
 
     // Resetiranje forme
     document.getElementById("kontaktForm").reset();
